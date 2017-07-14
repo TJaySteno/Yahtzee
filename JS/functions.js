1045a42dbb9c8d4f446ca42e18b51c14f2b41617
Trent Stenoien <trent.stenoien@gmail.com>
2017-07-14 00:03:07 -0500

    Added pseudo code for htmlElements.js

--- JS/functions.js
+++ JS/functions.js
@@ -1,10 +1,50 @@
-let dice = [];
-
 const d = () => Math.floor(Math.random() * 6 ) + 1;
 
 const rollDice = () => {
 	dice = [];
-	for (let i = 0; i < 6; i++) {
+	for (let i = 0; i < 5; i++) {
 		dice.push(d());
-	}
-}
+	};
+};
+
+
+//rollDice()
+	//get html
+	//roll dice
+	//sort
+	//write dice to buttons
+	
+//roll()
+	//forEach
+		//if 'rerolling'
+			//roll
+			//print roll
+
+//updateScores()
+	//get html
+		//upper
+		//lower
+		//total
+	//calculate totals
+	//update html
+
+//score()
+	//scoringFunction()
+	//updateScores()
+
+//updateRoundCounter()
+	//get html
+	//count rounds
+	//display rounds
+
+//updateRollButton()?
+	//get html
+	//print rolls
+
+//newRound()
+	//round++
+	//rolls = 3
+	//updateRoundCounter()
+	//updateRollButton()
+
+//endGame()
--- JS/htmlElements.js
+++ JS/htmlElements.js
@@ -1,1 +1,24 @@
-//html
+/**********************************************************
+VARIABLES
+**********************************************************/
+
+//Find page elements
+const roundCounter = document.querySelector('#roundCounter');
+const diceDiv = document.querySelector('.dice');
+const diceButtons = Array.from(diceDiv.children);
+const rollButton = document.querySelector('#rollButton');
+const scoreButton = rollButton.nextElementSibling;
+const radioInput = document.querySelectorAll('input');
+const upperScore = document.querySelector('#upperScore');
+const lowerScore = document.querySelector('#lowerScore');
+const totalScore = document.querySelector('#totalScore');
+
+//Create variables
+let dice = [];
+let rollsLeft = 2;
+
+//Event listeners
+	//Dice buttons
+	//Roll button
+	//Score button
+	//Radio buttons?
