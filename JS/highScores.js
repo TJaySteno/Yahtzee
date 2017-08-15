b7ffcadf4ec81617d571337636c98e6fa80f261b
TJaySteno <trent.stenoien@gmail.com>
2017-08-15 14:44:43 -0500

    Most recent versions for review and bug-fixing

--- JS/highScores.js
+++ JS/highScores.js
@@ -1,1 +1,15 @@
 //High scores
+const highScores = (finalScore, name) => {
+	if (localStorage.highScore < finalScore) {
+		localStorage.highScore = finalScore;
+		localStorage.nombre = name;
+	};
+};
+
+	//localStorage
+		//Create array high scores
+			//Track top 10
+				//Add score to array
+				//Sort
+				//pop(?)
+			
