ffb53dabad5ef6a491466123f90241fb1481d519
Trent Stenoien <trent.stenoien@gmail.com>
2018-03-06 16:59:49 -0600

    Fixed table styling, corrected a bug with scoring dates, and added a High Scores fieldset for easier viewing

---
+++ JS/scoring.js
@@ -0,0 +1,143 @@
+//Store scores
+const upperScores = {};
+const lowerScores = {};
+const total = {};
+
+//Yahtzee variables
+let preventYahtzee = false;
+let overwriteYahtzee = false;
+
+//Fundamental functions
+const is = (num) => dice.filter((value) => value === num);
+	//Return an array with every instance of 'num'
+
+const sum = (arr) => arr.reduce(( acc, cur) => acc + cur, 0);
+	//Add the sum of an array
+
+//Scoring functions
+const scoreNum = (num, id) => {
+	//Score upper scores
+	let filtered = is(num);
+	upperScores[id] = sum(filtered);
+	newRound();
+};
+
+function scoreSets(oak, fh, id) {
+	//Score 3, 4, and 5 of a kind
+	dice.sort();
+	let j = 0;
+	let k = 0;
+	let d = 0;
+	//Check for sets
+	for (let i = 0; i < dice.length; i++) {
+		if ( dice[i] === dice[i+1] && d === 0 ) {
+			j++;
+			d = dice[i];
+		} else if ( dice[i] === dice[i+1] && d === dice[i] ) {
+			j++;
+		} else if ( dice[i] === dice[i+1] && d !== dice[i] ) {
+			k++;
+		};
+	};
+	//Evaluate sets
+	if (fh && (( j === 1 && k === 2 ) || ( j === 2 && k === 1 ))) {
+		//Score fullHouse
+		lowerScores[id] = 25;
+		newRound();
+	} else if (oak === 4 && j === 4) {
+		//Score Yahtzee
+		scoreYahtzee(id);
+	} else if (!fh && j >= oak) {
+		//Score 3 or 4 of a kind
+		lowerScores[id] = sum(dice);
+		newRound();
+	} else {
+		//Failed to score a set
+		if (!lowerScores[id]) {
+			lowerScores[id] = 0;
+		} else {
+			overwriteYahtzee = true;
+		};
+		newRound();
+	}
+};
+
+const scoreYahtzee = (id) => {
+	//Score Yahtzee
+	if (!lowerScores.yahtzee) {
+		//Score first Yahtzee
+		lowerScores[id] = 50;
+		newRound();
+	} else if (preventYahtzee) {
+		//Prevent 2 Yahtzees from being scored with the same dice
+		alert("Don't be greedy bro! Please pick something besides another Yahtzee.");
+	} else {
+		//Score second Yahtzee
+		lowerScores[id] += 100;
+		preventYahtzee = true;
+		alert('Congrats! Select a second scoring option. The upper section is scored per usual, the lower section is always considered passing.');
+	};
+};
+
+function scoreRuns(len, score, id) {
+	//Score small and large straights
+	dice.sort();
+	let j = 0;
+	//Check for run
+	for (let i = 0; i < dice.length; i++) {
+		if ( dice[i]+1 === dice [i+1] ) {
+			j++;
+		};
+	};
+	//Evaluate run
+	if ( j >= len ) {
+		//Scored a run
+		lowerScores[id] = score;
+	} else {
+		//Failed to score a run
+		lowerScores[id] = 0;
+	}
+	newRound();
+};
+
+const score = {
+	//Call scoring functions
+	one: () => scoreNum(1, 'one'),
+	two: () => scoreNum(2, 'two'),
+	three: () => scoreNum(3, 'three'),
+	four: () => scoreNum(4, 'four'),
+	five: () => scoreNum(5, 'five'),
+	six: () => scoreNum(6, 'six'),
+	
+	threeOAK: () => scoreSets(2, false, 'threeOAK'),
+	fourOAK: () => scoreSets(3, false, 'fourOAK'),
+	fullHouse: () => scoreSets(2, true, 'fullHouse'),
+	sStraight: () => scoreRuns(3, 30, 'sStraight'),
+	lStraight: () => scoreRuns(4, 40, 'lStraight'),
+	yahtzee: () => scoreSets(4, false, 'yahtzee'),
+	chance: () => {
+		lowerScores.chance = sum(dice);
+		newRound();
+	}
+};
+
+const addScore = (obj) => {
+	//Add and return scores in 'obj'
+	let sum = 0;
+	for (let prop in obj) {
+		sum += obj[prop];
+	};
+	return sum;
+};
+
+const getTotal = () => {
+	//Add and store current points
+	total.upper = addScore(upperScores);
+	if ( total.upper >= 63 && !upperScores.bonus ) {
+		upperScores.bonus = 35;
+		bonus.textContent = upperScores.bonus;
+		total.upper = addScore(upperScores);
+	};
+	total.lower = addScore(lowerScores);
+	total.total = total.upper + total.lower;
+};
---
+++ README.md
@@ -0,0 +1,8 @@
+Welcome to Yahtzee!
+
+I created this app as a challenge for myself in the first couple of months I started learning JavaScript (March 2017) and the first version was complete two weeks later. What you see here is the second version of that app which has been refactored to be easier to read and DRY.
+
+My focus with this project and my learning in general has been the JavaScript. At some point I may improve the webpage design, but for now just know that my focus has been on functionality of this and other apps.
+
+Thank you and enjoy!
+Trent Stenoien
